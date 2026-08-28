464567428f56f9c3a3ba9a802275a1e42e862c25
saidia-coder <abdellaoui24@hotmail.com>
2026-08-28 21:09:40 +0200

    Add WhatsApp icon and update phone link

--- components/PropertyGrid.js
+++ components/PropertyGrid.js
@@ -1,7 +1,16 @@
 "use client";
 
 import React, { useState, useMemo } from "react";
-import { MapPin, Search, BedDouble, Bath, Maximize2, X, MessageCircle, Waves, TreePine, Building2, ArrowUpRight, Users, FileCheck, Handshake, PhoneCall } from "lucide-react";
+import { MapPin, Search, BedDouble, Bath, Maximize2, X, MessageCircle, Waves, TreePine, Building2, ArrowUpRight, Users, FileCheck, Handshake } from "lucide-react";
+
+function WhatsAppIcon({ size = 15 }) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.148-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347z"/>
+      <path d="M12.001 2C6.478 2 2 6.477 2 12c0 1.876.52 3.632 1.42 5.13L2 22l4.998-1.396A9.955 9.955 0 0012.001 22C17.523 22 22 17.523 22 12S17.523 2 12.001 2zm0 18.267a8.24 8.24 0 01-4.437-1.29l-.318-.19-3.03.847.836-2.955-.207-.303a8.238 8.238 0 01-1.31-4.442c0-4.556 3.71-8.267 8.267-8.267 4.556 0 8.267 3.71 8.267 8.267 0 4.556-3.71 8.267-8.267 8.267z"/>
+    </svg>
+  );
+}
 
 const WHATSAPP_NUMBER = "31614729973";
 
@@ -174,7 +183,7 @@ export default function PropertyGrid({ properties, regions, fetchError }) {
           rel="noopener noreferrer"
           style={{ display: "flex", alignItems: "center", gap: "6px", fontSize: "13px", fontWeight: 600, color: "var(--orange)", textDecoration: "none" }}
         >
-          <PhoneCall size={15} /> +{WHATSAPP_NUMBER.replace(/(\d{2})(\d{1})(\d{8})/, "$1 $2 $3")}
+          <WhatsAppIcon size={15} /> +{WHATSAPP_NUMBER.replace(/(\d{2})(\d{1})(\d{8})/, "$1 $2 $3")}
         </a>
       </header>
 
